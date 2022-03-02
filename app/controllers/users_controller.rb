85a32381f1747653d35fc2405b0a1d07442a0e8a
Nikunj Bacancy <nikunj.padhiyar@bacancy.com>
2022-03-02 16:46:08 +0530

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Alpha blog #{@user.username}. You signed up successfully!"
+            redirect_to article_path
+        else
+            render 'new'
+        end
+    end
+
+    
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
